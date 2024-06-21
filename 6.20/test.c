cefa7eb7cb40a39930a67bfc687bf112a1a3879c
shanmu-W <3096612410@qq.com>
2024-06-21 15:33:27 +0800

    Add files via upload

---
+++ 6.20/test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int br(void);
+int ic(void);
+int main(void)
+{
+	br();
+	printf("");
+	ic();
+	printf("\n");
+	ic();
+	printf( "\n" );
+	br();
+	printf("\n");
+	return 0;
+}
+int br(void)
+{
+	printf("ÏÄ");
+}
+int ic(void)
+{
+	printf("ÑÏ\n");
+}
